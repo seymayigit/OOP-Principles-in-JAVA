a4dc9bcc015dcb98c2791ef5dc39c7abddce9917
Hatice Seyma YIGIT <seymayigitbm@gmail.com>
2020-12-01 00:06:31 +0300

    Encapculation is added

---
+++ OOP/src/encapsulation/MainClass.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+
+		int personId = person.personelId;
+		String name = person.getName();
+	}
+
+}
---
+++ OOP/src/encapsulation/Person.java
@@ -0,0 +1,21 @@
+package encapsulation;
+
+public class Person {
+
+	private String name;
+	public int personelId;
+	
+	private void mat() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
